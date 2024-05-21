3c11c6bc34609a3ea7dba767b4448c7025d6c2c8
Victor Zimmer <victor.zimmer@me.com>
2024-05-21 14:06:33 +0200

    Added TailwindCSS.

--- reportix-vue/src/App.vue
+++ reportix-vue/src/App.vue
@@ -6,7 +6,9 @@ import TheWelcome from './components/TheWelcome.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
