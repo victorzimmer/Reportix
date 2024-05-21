43ae1ca6e6e430e0b1e4d1d647b50443577542a2
Danielius Stanevicius <daniel@tromso-ansatte1-822.wifi.uit.no>
2024-05-21 18:57:33 +0200

    Added: full width TextFieldComponent textarea

--- reportix-vue/src/components/DocumentEditor.vue
+++ reportix-vue/src/components/DocumentEditor.vue
@@ -3,22 +3,39 @@ import TextFieldComponent from './TextFieldComponent.vue';
 </script>
 
 <template>
-  <div class="documentEditor overflow-y-scroll">
+  <div class="documentEditor">
     <h1 class="green">Document editor</h1>
     <h3>This area will contain editor.</h3>
-    <div class="">
-      <div class="">
+    <div class="align-center">
+      <div class="text-field-container">
         <TextFieldComponent msg="Introduction"/>
       </div>
-      <div class="">
+      <div class="text-field-container">
         <TextFieldComponent msg="Method"/>
       </div>
-      <div class="">
+      <div class="text-field-container">
         <TextFieldComponent msg="Result"/>
       </div>
-      <div class="">
+      <div class="text-field-container">
         <TextFieldComponent msg="Discussion"/>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.documentEditor {
+  text-align: center;
+}
+
+.align-center {
+  display: contents;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.text-field-container {
+  margin: 10px 0;
+}
+</style>
--- reportix-vue/src/components/TextFieldComponent.vue
+++ reportix-vue/src/components/TextFieldComponent.vue
@@ -5,9 +5,8 @@ defineProps<{
 </script>
 
 <template>
-  <div class="textFieldComponent overflow-y-scroll">
+  <div class="textFieldComponent pt-10 w-full">
     <h1 class="green">{{ msg }}</h1>
-    <h3>This is {{ msg }} text field</h3>
-    <textarea placeholder="Bio" class="textarea textarea-bordered textarea-lg w-full max-w-xs" ></textarea>
+    <textarea placeholder="Bio" class="textarea textarea-bordered textarea-lg w-full " ></textarea>
   </div>
 </template>
