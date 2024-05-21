7bbf94c2bb900131fb566c44b6affe1197b94826
Victor Zimmer <victor.zimmer@me.com>
2024-05-21 13:45:48 +0200

    Added structure for static files.

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from typing import Union
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse, FileResponse
 
 app = FastAPI()
 
 
-@app.get("/", response_class=HTMLResponse)
-@app.get("/index.html", response_class=HTMLResponse)
+@app.get("/", response_class=FileResponse)
+@app.get("/index.html", response_class=FileResponse)
 def read_root():
-    index = open("client/index.html")
-    return index.read()
+    return "client/index.html"
+
+@app.get("/static/{filename}", response_class=FileResponse)
+def read_static_file(filename: str):
+    return "client/"+filename
 
 
-@app.get("/update/{field_id}")
+
+
+@app.get("/textfield/{field_id}", response_class=HTMLResponse)
 def read_item(field_id: str):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": field_id}
+
+
+# @app.update("/textfield/{field_id}", response_class=HTMLResponse)
+# def read_item(field_id: str):
+#     return {"item_id": field_id}
